29057392be271a9c96f38426548b713725b3e24f
Miguel Fontes <miguel_m_fontes@hotmail.com>
2019-03-12 22:35:56 -0300

    New aliases

--- aliases/git.sh
+++ aliases/git.sh
@@ -6,6 +6,7 @@ alias st='git status'
 alias cln='git clone'
 alias cmm='git commit -m'
 alias psh='git push'
+alias psh+=$'git branch | grep "*" | awk \'{ print $2 }\' | xargs git push --set-upstream origin' # Push a branch for the first time
 alias chk='git checkout'
 alias chk+='git checkout -b'
 alias graph='git log --graph'
@@ -17,3 +18,5 @@ alias m='git merge'
 alias t='git tag'
 alias t+='git tag -a'
 alias t-='git tag -d'
+alias cpb=$'git branch | grep "*" | awk \'{ print $2 }\' | pbcopy' # Copy branch name
+
