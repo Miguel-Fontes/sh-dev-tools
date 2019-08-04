68ad05b3290e15dca35374e24ad5cf48d6a28647
Miguel <miguel_m_fontes@hotmail.com>
2019-08-04 17:04:22 -0300

    Adicionadas novas funcionalidades

--- git/gitter.sh
+++ git/gitter.sh
@@ -11,10 +11,14 @@ USAGE: $(basename "$0") [OPTIONS]
        $(basename "$0") -s -e my-source-file.java my-resource.properties
 
 Available options:
-    -s, --stage     Stage all changed files, or a subset of those filtered by a pattern provided by the --exclude option.
-    -e, --exclude   Excludes files from operations. This filter is made using a regex expression with grep. Adittionaly,
-                    any number of exclusion can be defined: they will be combine with an OR ( PATTERN | PATTERN | PATTERN ).
-    -h, --help      Prints this help message.
+    -c, --commit [MESSAGE]  Commits the current staged files with the given message.
+    -p, --push              Pushes the branch to origin.
+    -s, --status            Show the current changed files.
+    -r, --reset-all         Clear the staged files.
+    -t, --stage             Stage all changed files, or a subset of those filtered by a pattern provided by the --exclude option.
+    -e, --exclude           Excludes files from operations. This filter is made using a regex expression with grep. Adittionaly,
+                            any number of exclusion can be defined: they will be combine with an OR ( PATTERN | PATTERN | PATTERN ).
+    -h, --help              Prints this help message.
 "
 
 # Verify dependencies
@@ -44,18 +48,47 @@ stageFiles() {
     git stage $FILES_TO_STAGE
 }
 
+status() {
+    git status
+}
+
+reset() {
+    git reset HEAD -- .
+}
+
+commit() {
+    git commit -m "$COMMIT_MESSAGE"
+}
+
+push() {
+    git branch | grep "*" | awk '{ print $2 }' | xargs git push --set-upstream origin
+}
 # Prints a help message if no argument was given
 test "${#}" -eq 0 && (help && exit 0)
 
 while [[ -n "$1" ]]; do
     case "$1" in
-    -s | --stage)
+    -s | --status)
+        STATUS='true'
+        ;;
+    -t | --stage)
         STAGE='true'
         ;;
     -e | --exclude)
         EXCLUDES='true'
         FILES_TO_EXCLUDE+="$2 "
         shift
+        ;;
+    -r | --reset-all)
+        RESET='true'
+        ;;
+    -c | --commit)
+        COMMIT='true'
+        COMMIT_MESSAGE="$2"
+        shift
+        ;;
+    -p | --push)
+        PUSH='true'
         ;;
     -h | --help)
         help
@@ -67,3 +100,7 @@ while [[ -n "$1" ]]; do
 done
 
 test "$STAGE" == 'true' && stageFiles
+test "$STATUS" == 'true' && status
+test "$RESET" == 'true' && reset
+test "$COMMIT" == 'true' && commit
+test "$PUSH" == 'true' && push
